d8960a289b157c9371da97cbaac8253ad37bba46
David Čadež <david.cadez89@gmail.com>
2022-12-20 10:55:47 +0100

    fixed fetch issue

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -28,7 +28,7 @@ function Deck() {
   const [objs, setObjs] = useState([]);
 
   const fetchData = async () => {
-    fetch("http://localhost:8082/product/get")
+    fetch("http://192.168.27.100:8082/product/get")
       .then((response) => response.json())
       .then((data) => {
         setObjs(data);
